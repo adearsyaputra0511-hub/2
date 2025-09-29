5d2239185f55f791f8269722a4790d02c243f578
adearsyaputra0511-hub <adearsyaputra0511@gmail.com>
2025-09-29 14:48:29 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
